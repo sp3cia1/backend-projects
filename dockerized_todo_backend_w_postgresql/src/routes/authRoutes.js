3a223af7f999c70957f38ca21fcf9dbc3f735d11
Pranjal Upadhyay <pranjalupadhyay13@gmail.com>
2025-02-02 21:12:36 +0530

    updated the auth and todo routes to use prisma

--- dockerized_todo_backend_w_postgresql/src/routes/authRoutes.js
+++ dockerized_todo_backend_w_postgresql/src/routes/authRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
+import prisma from './prismaClient.js'
 import db from '../db.js'
 
 //The intercepted request is sent here by the middleware and then the router looks for matching route handler
 const router = express.Router()
 
-router.post('/register', (req,res) => {
+router.post('/register', async (req,res) => {
     const {username, password} = req.body
     //save the username and an irrevarsibly encypted password, so even if we get hacked the password in db remain safe
 
@@ -15,16 +16,25 @@ router.post('/register', (req,res) => {
 
     //save the new user and hashed password to the db
     try{
-        const insertUser = db.prepare(`INSERT INTO users (username, password) VALUES (?,?)`)
-        const result = insertUser.run(username, hashedPassword) //This method executes a prepared SQL statement and retrieves all rows from the result set. It is used when you expect multiple rows to be returned.
+        const user = await prisma.user.create({
+            data: {
+                username,
+                password: hashedPassword
+            }
+        })
 
         //now that we have a user, I want to add their first todo for them
         const defaultTodo = `Hello :) Add your first todo!`
-        const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES(?,?)`)
-        insertTodo.run(result.lastInsertRowid,defaultTodo)
+        
+        await prisma.todo.create({
+            data: {
+                task: defaultTodo,
+                userId: user.id
+            }
+        })
 
         //create a token
-        const token = jwt.sign({id: result.lastInsertRowid}, process.env.JWT_SECRET, { expiresIn: '24h'})
+        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, { expiresIn: '24h'})
         res.json({ token })
     } catch(err){
         console.log(err.message)
@@ -32,7 +42,7 @@ router.post('/register', (req,res) => {
     }
 })
 
-router.post('/login', (req,res) => {
+router.post('/login', async(req,res) => {
     // we get their emial, and we lookup the password associated with that email in the database.
     //but we get it back and we see its encrypted, which means that we cannot compare it to the one the user just used trying to login.
     // so what we can do, is again, one way encrypt the password the user just entered with the same key/algo that was used during registeration to get the saem encrypted password.
@@ -40,8 +50,11 @@ router.post('/login', (req,res) => {
     const {username, password} = req.body
 
     try{
-        const getUser = db.prepare('SELECT * FROM users WHERE username = ?')
-        const user = getUser.get(username) //retrieves a single row
+        const user = await.prisma.user.findUnique({
+            where: {
+                username: username
+            }
+        }) 
 
         //if we cannot find a user associated with the username return out of the function
         if (!user) { return res.status(404).send({message: "User not found"})}
--- dockerized_todo_backend_w_postgresql/src/routes/todoRoutes.js
+++ dockerized_todo_backend_w_postgresql/src/routes/todoRoutes.js
@@ -1,44 +1,63 @@
 import express from 'express'
 import db from '../db.js'
+import prisma from './prismaClient.js'
 
 const router = express.Router()
 
 //Get all the todos for the logged in user
-router.get('/', (req,res) => {
-    const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?')
-    const todos = getTodos.all(req.userId) //retreives all the matching rows
-
+router.get('/', async (req,res) => {
+    const todods = await prisma.todo.findMany({
+        where: {
+            userId: req.userId
+        }
+    })
     res.json(todos)
 })
 
 //Create a new todo
-router.post('/', (req,res) => {
-    const { task } = req.body
-    const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?,?)`)
+router.post('/', async (req,res) => {
+    const {task} = req.body
 
-    const result = insertTodo.run(req.userId, task)
+   const todo = await prisma.todo.create({
+        data:{
+            task,
+            userId: req.userId
+        }
+   })
 
-    res.json({id: result.lastInsertRowId, task, completed: 0})
+    res.json(todo)
 })
 
 //update a todo, here id is a dynamic query parameter
-router.put('/:id', (req,res) => {
+router.put('/:id', async (req,res) => {
     const { completed } = req.body
     const { id } = req.params
 
-    const updatedTodo = db.prepare(`UPDATE todos SET completed = ? WHERE id = ?`)
+    const updatedTodo = await prisma.todo.update({
+        where: {
+            id: parseInt(id),
+            userId: req.userId
+        },
+        data: {
+            completed: !!completed //forcing it to become the boolean version
+        }
+    })   
 
-    updatedTodo.run(completed, id)
-
-    res.json({message: "Todo Completed"})
+    res.json(updatedTodo)
 })
 
 //Delete a todo
-router.delete('/:id', (req,res) => {
+router.delete('/:id', async (req,res) => {
     const { id } = req.params
     const userId = req.userId
-    const deleteTodo = db.prepare(`DELETE FROM todos WHERE id = ? AND user_id = ?`)
-    deleteTodo.run(id, userId) 
+   
+    await prisma.todo.delete({
+        where: {
+            id: parseInt(id),
+            userId
+        }
+    })
+
     res.send({message: "Todo deleted"})
 })
 
